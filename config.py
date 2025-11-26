84950335dca08a21b9f4e838b7615be3b1a63fc6
widhvans <110589162+widhvans@users.noreply.github.com>
2025-11-26 13:57:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
-# config.py
+import os
 
-API_ID = 1234567  # my.telegram.org se milega
-API_HASH = "your_api_hash_here"
-BOT_TOKEN = "your_bot_token_here" # BotFather se milega
-MONGO_URL = "your_mongodb_connection_string" # MongoDB Atlas url
-OWNER_ID = 123456789 # Aapki khud ki numeric Telegram ID
+# my.telegram.org se lein
+API_ID = 38364162  # Replace with integer
+API_HASH = "6802312a466d3b34133ebd5de3374524"
+
+# BotFather se lein
+BOT_TOKEN = "8368477494:AAEX-ndxDVt9F_EyfMCQc8tAQNObH-TH51Y"
+
+# MongoDB connection string (MongoDB Atlas se lein)
+MONGO_URI = "mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority"
+
+# Aapki khud ki numeric ID (Security ke liye, taaki koi aur bot settings na change kare)
+OWNER_ID = 123456789
