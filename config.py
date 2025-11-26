6002a1188cc116348899aed4ad7e3e44ca5c9cde
widhvans <110589162+widhvans@users.noreply.github.com>
2025-11-26 14:51:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 import os
+from dotenv import load_dotenv
 
-# my.telegram.org se lein
-API_ID = 38364162  # Replace with integer
-API_HASH = "6802312a466d3b34133ebd5de3374524"
+load_dotenv()
 
-# BotFather se lein
-BOT_TOKEN = "8368477494:AAEX-ndxDVt9F_EyfMCQc8tAQNObH-TH51Y"
-
-# MongoDB connection string (MongoDB Atlas se lein)
-MONGO_URI = "mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority"
-
-# Aapki khud ki numeric ID (Security ke liye, taaki koi aur bot settings na change kare)
-OWNER_ID = 123456789
+API_ID = int(os.getenv("API_ID", "38364162")) # Apna API ID daalein
+API_HASH = os.getenv("API_HASH", "6802312a466d3b34133ebd5de3374524") # Apna API Hash daalein
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8368477494:AAEX-ndxDVt9F_EyfMCQc8tAQNObH-TH51Y") # Bot Father se mila token
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # MongoDB URL
+OWNER_ID = int(os.getenv("OWNER_ID", "8303569508")) # Aapki khud ki Telegram ID (admin control ke liye)
