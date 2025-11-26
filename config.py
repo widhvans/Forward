a3b718d705e79687512d8e469858824020d86581
widhvans <110589162+widhvans@users.noreply.github.com>
2025-11-26 13:46:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+API_ID = 1234567  # my.telegram.org se milega
+API_HASH = "your_api_hash_here"
+BOT_TOKEN = "your_bot_token_here" # BotFather se milega
+MONGO_URL = "your_mongodb_connection_string" # MongoDB Atlas url
+OWNER_ID = 123456789 # Aapki khud ki numeric Telegram ID
